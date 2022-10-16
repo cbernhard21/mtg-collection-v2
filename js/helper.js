562dd2165e2d9df4aa8c9f538fe9de6506b97e1a
Christopher Bernhard <58096490+cbernhard21@users.noreply.github.com>
2022-10-16 10:42:05 -0500

    worked on search box style/layout

--- js/helper.js
+++ js/helper.js
@@ -1,16 +1,12 @@
 export async function searchSingleCard(card) {
-    const url = 'https://api.magicthegathering.io/v1/cards?name=';
-    try {
-        const res = await fetch(`${url}${card}`);
-        const data = await res.json();
-        const singleCard = data.cards[0];
-        const cardCount = data.cards.length
-        console.log(cardCount);
-        console.log(singleCard);
-        return singleCard
-        
-
-    } catch (error) {
-        console.log(error)
-    }
+  const url = 'https://api.magicthegathering.io/v1/cards?name=';
+  try {
+    const res = await fetch(`${url}${card}`);
+    const data = await res.json();
+    const singleCard = data.cards[0];
+    const cardCount = data.cards.length;
+    return { singleCard, cardCount };
+  } catch (error) {
+    console.log(error);
+  }
 }
--- views/dashboard/index.js
+++ views/dashboard/index.js
@@ -1,5 +1,5 @@
 // HELPER FUNCTIONS IMPORTED FROM OTHER FILE
-import { searchSingleCard } from "../../js/helper.js";
+import { searchSingleCard } from '../../js/helper.js';
 
 // VARIABLES FOR DASHBOARD
 const cardName = document.querySelector('#card-name');
@@ -11,43 +11,45 @@ const resultsContainer = document.querySelector('#results-container');
 
 // EVENT LISTENER FOR THE SEARCH FORM IN CASE THE USER PRESSES THE ENTER KEY
 searchForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    renderCard(searchSingleCard, cardName.value, resultsContainer)
-    cardName.value = '';
-})
+  e.preventDefault();
+  renderCard(searchSingleCard, cardName.value, resultsContainer);
+  cardName.value = '';
+});
 
-// EVENT LISTENER FOR THE SEARCH BUTTON 
+// EVENT LISTENER FOR THE SEARCH BUTTON
 searchBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    renderCard(searchSingleCard, cardName.value, resultsContainer)
-    cardName.value = '';
-})
-
+  e.preventDefault();
+  renderCard(searchSingleCard, cardName.value, resultsContainer);
+  cardName.value = '';
+});
 
 // FUNCTIONS
 async function renderCard(searchFunc, searchedCard, htmlContainer) {
-    let card = await searchFunc(searchedCard);
-    console.log(card);
-    let cardImage = insert(card.imageUrl, 's', 4);
-    let html = `
-        <div class="card-info-container">
-            <img src="${cardImage}" alt="${card.name}" class="card-image" />
-            <ul>
-                <li>${card.name}</li>
-                <li>${card.manaCost}</li>
-            </ul>
-        </div>
+  let card = await searchFunc(searchedCard);
+  let singleCard = card.singleCard;
+  let cardCount = card.cardCount;
+
+  let cardImage = insert(singleCard.imageUrl, 's', 4);
+  let searchResultHtml = `
+ 
+        <img src="${cardImage}" alt="${singleCard.name}" class="card-image" />
+        <!-- <ul> 
+            <li>${singleCard.name}</li>
+            <li>${singleCard.manaCost}</li>
+        </ul> -->
+
+        <p class="search-card-text">There are ${cardCount} different ${searchedCard} card(s).</p>
         <a href="/views/search" id="results-link" class="btn results-link">See All Results</a>
     `;
-    htmlContainer.innerHTML = html;
+  htmlContainer.innerHTML = searchResultHtml;
 
-    const resultsLink = document.querySelector('#results-link');
-    resultsLink.addEventListener('click', () => {
-        console.log('i was clicked');
-    })
+  const resultsLink = document.querySelector('#results-link');
+  resultsLink.addEventListener('click', () => {
+    console.log('i was clicked');
+  });
 }
 
 // INSERT THE LETTER 'S' TO MAKE A HTTPS REQUEST FOR IMAGE (ORIGINALLY HTTP)
 function insert(mainString, insString, pos) {
-    return mainString.slice(0, pos) + insString + mainString.slice(pos);
+  return mainString.slice(0, pos) + insString + mainString.slice(pos);
 }
