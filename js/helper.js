499784bddd8f5f091ab5e48420e3c2e35ed0cc2e
Christopher Bernhard <chris_bernhard@yahoo.com>
2022-10-16 01:36:18 +0000

    WORKING ON SEARCH

--- js/helper.js
+++ js/helper.js
@@ -1,3 +1,15 @@
-export function searchCards(card) {
+export async function searchSingleCard(card) {
     console.log(card);
+    const url = 'https://api.magicthegathering.io/v1/cards?name=';
+
+    try {
+        const res = await fetch(`${url}${card}`);
+        const data = await res.json();
+        const singleCard = data.cards[0];
+        console.log(singleCard);
+        return singleCard
+
+    } catch (error) {
+        console.log(error)
+    }
 }
--- views/dashboard/index.js
+++ views/dashboard/index.js
@@ -1,5 +1,5 @@
 // HELPER FUNCTIONS
-import { searchCards } from "../../js/helper.js";
+import { searchSingleCard } from "../../js/helper.js";
 
 // VARIABLES FOR DASHBOARD
 const cardName = document.querySelector('#card-name');
@@ -10,8 +10,31 @@ const resultsContainer = document.querySelector('#results-container');
 searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    searchCards(cardName.value);
-    
+    renderCard(searchSingleCard, cardName.value, resultsContainer)
 
+    // renderCard(searchSingleCard, cardName.value, resultsContainer);
     cardName.value = '';
 })
+
+
+
+
+async function renderCard(searchFunc, searchedCard, htmlContainer) {
+    let card = await searchFunc(searchedCard);
+    let cardImage = insert(card.imageUrl, 's', 4);
+    let html = `
+        <div class="card-info-container">
+            <img src="${cardImage}" alt="${card.name}" class="card-image" />
+            <ul>
+                <li>${card.name}</li>
+                <li>${card.manaCost}</li>
+            </ul>
+        </div>
+    `;
+    htmlContainer.innerHTML = html;
+}
+
+// INSERT THE LETTER 'S' TO MAKE A HTTPS REQUEST FOR IMAGE (ORIGINALLY HTTP)
+function insert(mainString, insString, pos) {
+    return mainString.slice(0, pos) + insString + mainString.slice(pos);
+}
