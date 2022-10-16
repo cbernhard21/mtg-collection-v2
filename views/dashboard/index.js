c7734b4791f402e4bde1528434cca26d8e7f961a
Christopher Bernhard <chris_bernhard@yahoo.com>
2022-10-16 02:09:17 +0000

    style search container

--- views/dashboard/index.js
+++ views/dashboard/index.js
@@ -16,8 +16,11 @@ searchForm.addEventListener('submit', (e) => {
     cardName.value = '';
 })
 
-
-
+searchBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    renderCard(searchSingleCard, cardName.value, resultsContainer)
+    cardName.value = '';
+})
 
 async function renderCard(searchFunc, searchedCard, htmlContainer) {
     let card = await searchFunc(searchedCard);
@@ -30,8 +33,14 @@ async function renderCard(searchFunc, searchedCard, htmlContainer) {
                 <li>${card.manaCost}</li>
             </ul>
         </div>
+        <a href="/views/search" id="results-link" class="btn results-link">See All Results</a>
     `;
     htmlContainer.innerHTML = html;
+
+    const resultsLink = document.querySelector('#results-link');
+    resultsLink.addEventListener('click', () => {
+        console.log('i was clicked');
+    })
 }
 
 // INSERT THE LETTER 'S' TO MAKE A HTTPS REQUEST FOR IMAGE (ORIGINALLY HTTP)
